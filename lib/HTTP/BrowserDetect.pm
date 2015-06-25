32f8fd02ed7c8e1cc7b05e1b65b7e8eb4879525e
Olaf Alders <olaf@wundersolutions.com>
2015-06-24 23:26:25 -0400

    Fix test failures on Perl 5.8

--- lib/HTTP/BrowserDetect.pm
+++ lib/HTTP/BrowserDetect.pm
@@ -324,7 +324,7 @@ sub new {
         $user_agent = $ENV{'HTTP_USER_AGENT'};
     }
 
-    $self->{user_agent} = $user_agent // '';
+    $self->{user_agent} = defined $user_agent ? $user_agent : '';
     $self->_init_core;
 
     return $self;
