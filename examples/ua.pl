4cc460563ce94ca94f6bc6d80367a5660f3f0c98
Olaf Alders <olaf@wundersolutions.com>
2017-12-13 21:58:28 -0500

    Tidy example script

--- examples/ua.pl
+++ examples/ua.pl
@@ -7,6 +7,7 @@ use HTTP::BrowserDetect ();
 my $ua = shift || die qq{Usage: perl $0 "my UA string"};
 my $h = HTTP::BrowserDetect->new($ua);
 
-printf "robot: %s\n",      $h->robot      || q{ };
-printf "robot name: %s\n", $h->robot_name || q{ };
-printf "robot id: %s\n",   $h->robot_id   || q{ };
+printf "robot: %s\n",       $h->robot        || q{ };
+printf "robot name: %s\n",  $h->robot_name   || q{ };
+printf "robot string %s\n", $h->robot_string || q{ };
+printf "robot id: %s\n",    $h->robot_id     || q{ };
